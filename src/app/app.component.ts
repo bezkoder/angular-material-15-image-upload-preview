7fd9c6af43bdec463f5ba08cca8ae8e3b463d39a
tienbku <tien.ngtruong@gmail.com>
2022-12-22 14:24:58 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-material-15-image-upload-preview';
+  title = 'Angular Material 15 Image Upload with Preview';
 }
